d8c40abe2676fca4648cd3abde93d0e058ac129a
Maxime Killinger <killin_m@MacBook-Pro-de-Maxime.local>
2021-10-04 15:06:07 +0200

    Update dockerfile with alpine based image and config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	SIZE_LIMIT: 50, // mentioned in KB
+	SIZE_LIMIT: 5000, // mentioned in KB
 	PORT: process.env.PORT || 3000,
-	MONGO_URL: process.env.MONGODB_URI || "mongodb://localhost:27017/jsonbox-io-dev",
-	REQUEST_LIMIT_PER_HOUR: 100,
+	MONGO_URL: process.env.MONGODB_URI || "mongodb://192.168.1.118:27017/jsonbox-io-dev",
+	REQUEST_LIMIT_PER_HOUR: 300,
 	ENABLE_DATA_EXPIRY: false, // Once switched on the index will be be set in mongodb. Might need to remove it in order to switch off the behaviour
 	DATA_EXPIRY_IN_DAYS: 30,
 	FILTER_IP_SET: [], // example ['172.29.0.1']
